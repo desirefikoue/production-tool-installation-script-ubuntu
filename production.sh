2dc8b1adee16935aa70eeaeaba601ff0b9739cc7
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-10-29 19:09:42 -0400

    Update and rename centos.sh to production.sh

--- production.sh
+++ production.sh
@@ -4,7 +4,6 @@ sudo apt -y update
 
 sudo apt update -y
 
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 
 sudo cat /etc/system-release
 
@@ -26,30 +25,37 @@ sudo apt install -y git
 amazon-linux-extras install epel
 sudo apt update -y
 sudo apt install ansible -y
+sudo apt update -y
 sudo apt install nginx -y
-sudo apt install python-pip -y  
-sudo apt install awscli 
-sudo apt install boto -y 
+sudo ufw app list
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status
+systemctl status nginx
+curl -4 icanhazip.com
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle-1.16.312.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+sudo /usr/local/bin/python3.7 awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+aws --version
 cd /home 
 touch hug.yml 
 touch nat.yml 
 
 echo " Install Terraform"
-sudo apt install update -y 
-sudo wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip 
-sudo unzip terraform_1.1.2_linux_amd64.zip
-sudo mv terraform /usr/bin/
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
 terraform --version
 
 echo "Install Docker engine"
 sudo apt update -y
 sudo apt install docker -y
-systemctl start docker
-systemctl enable docker
-systemctl status docker
+sudo service docker start
+sudo service docker enable
+sudo service docker status
 #sudo usermod -a -G docker jenkins
 #sudo service docker start
-sudo chkconfig docker on
 
 echo " install Alpine"
 
